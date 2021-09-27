ed9b63b10d4e0eb458df618560d602d3f5cb2eeb
UXst <45309084+UXst@users.noreply.github.com>
2021-09-27 20:24:55 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -4,6 +4,7 @@ const $ = new Env();
 //注：此处设置github action用户填写到Settings-Secrets里面(Name输入PUSH_KEY)
 let SCKEY = '';
 
+
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
 //注：此处设置github action用户填写到Settings-Secrets里面（Name输入BARK_PUSH）
